efc169b19263cabf8f4ef6e7f17226399d5a06d0
Nico De Witte <nico.dewitte@outlook.com>
2019-01-08 09:06:08 +0100

    Simple server that sends a command when PLC connects

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # MQTT PLC Commander
 
-Command the Tilt Table from an MQTT app.
+Command the Tilt Table from MQTT.
+
+## NetCat test server
+
+You can create a test server using the following netcat command:
+
+```shell
+nc -kl 1234
+```
---
+++ app.js
@@ -0,0 +1,17 @@
+const net = require('net');
+let port = 1234;
+
+// Start a TCP Server
+net.createServer(function (socket) {
+  console.log("PLC Client connected to server");
+
+  let data = new Buffer([0x10, -1, -2]);
+  socket.write(data);
+
+  socket.on('end', function () {
+    console.log("PLC Client disconnected");
+  });
+  
+}).listen(port);
+
+console.log("MQTT PLC Commander is listening on port " + port);
